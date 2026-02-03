4dc463941f739ab34b94ecb5ff5237d47940235a
Dagin34 <dagmawinapoleon02@gmail.com>
2026-02-03 20:26:10 +0300

    Bark analysis

--- coding-contest-2/C_Bark_to_Unlock.py
+++ coding-contest-2/C_Bark_to_Unlock.py
@@ -5,13 +5,13 @@ words = []
 for _ in range(n):
   words.append(input())
 
-found = False
 starts_with = False
 ends_with = False
 
 for word in words:
   if word == password:
-    found = True
+    print("YES")
+    exit()
     
   if word[1] == password[0]:
     ends_with = True
@@ -19,7 +19,7 @@ for word in words:
   if word[0] == password[1]:
     starts_with = True
         
-if found or (ends_with and starts_with):
+if (ends_with and starts_with):
   print("YES")
 else:
   print("NO")
