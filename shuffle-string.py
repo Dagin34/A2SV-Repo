1799d8a4e17b1a162b6abf125d507d45d76c1c7b
Dagin34 <dagmawinapoleon02@gmail.com>
2026-02-18 22:12:44 +0300

    daily

--- shuffle-string.py
+++ shuffle-string.py
@@ -1,7 +1,5 @@
 class Solution:
-    def restoreString(s: str, indices: list[int]) -> str:
-        final = ''
-        for i in range(max(indices) + 1):
-            final += s[indices.index(i)]
+    def restoreString(self, s: str, indices: list[int]) -> str:
+        final = ''.join([s[indices.index(i)] for i in range(max(indices) + 1)])
         
         return final
