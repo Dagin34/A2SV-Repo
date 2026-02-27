7085c24074ce4f5b8a74430b2c82a1bdba912080
Dagin34 <dagmawinapoleon02@gmail.com>
2026-02-27 22:55:06 +0300

    pancake

---
+++ pancake.flip.py
@@ -0,0 +1,20 @@
+def pancakeSort(arr: list[int]) -> list[int]:
+    res = []
+    n = len(arr)
+
+    for size in range(n, 1, -1):
+        max_index = arr.index(max(arr[:size]))
+
+        if max_index == size - 1:
+            continue
+
+        if max_index != 0:
+            res.append(max_index + 1)
+            arr[:max_index + 1] = reversed(arr[:max_index + 1])
+
+        res.append(size)
+        arr[:size] = reversed(arr[:size])
+
+    return res
+
+print(pancakeSort([3,2,4,1]))
