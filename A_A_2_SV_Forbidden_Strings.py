0257341d35de729cb7d31b7ec27090581d0510d4
Dagin34 <dagmawinapoleon02@gmail.com>
2026-04-24 23:47:45 +0300

    daily

--- A_A_2_SV_Forbidden_Strings.py
+++ A_A_2_SV_Forbidden_Strings.py
@@ -1,18 +1,19 @@
 n = int(input())
 target = ['A','S','V']
 
-def find(initial, word, word2):
+# S V A
+def find(initial, last_word, second_from_last):
     if initial == n:
         return 1
 
     answer = 0
     for char in target:
-        if char == word:
+        if char == last_word:
             continue
-        if word2 == "S" and word == "V" and char == "A":
+        if second_from_last == "S" and last_word == "V" and char == "A":
             continue
 
-        answer += find(initial + 1, char, word)
+        answer += find(initial + 1, char, last_word)
     
     return answer
     
